a916a7e786506139e4ee9dde711fef41bc51fd8d
Herve THOMAS <herve.thomas@labside.fr>
2014-05-30 17:28:36 +0200

    Add Enterprise Post Type

--- core/post-types.php
+++ core/post-types.php
@@ -32,6 +32,42 @@ if (!class_exists('LS_Manager_Post_Types')) {
             
             global $ls_manager, $ls_manager_globals;
             
+            // Post Type : Enterprise
+            $labels = array(
+                'name'               => __('Enterprises',$ls_manager->ls_manager_domain),
+                'singular_name'      => __('Enterprise',$ls_manager->ls_manager_domain), 
+                'add_new'            => __('New enterprise',$ls_manager->ls_manager_domain),
+                'add_new_item'       => __('Add a new enterprise',$ls_manager->ls_manager_domain), 
+                'edit'               => __('Edit',$ls_manager->ls_manager_domain), 
+                'edit_item'          => __('Edit enterprise',$ls_manager->ls_manager_domain),
+                'new_item'           => __('New enterprise',$ls_manager->ls_manager_domain), 
+                'view'               => __('Enterprise',$ls_manager->ls_manager_domain), 
+                'view_item'          => __('View enterprise',$ls_manager->ls_manager_domain),
+                'search_items'       => __('Search enterprise',$ls_manager->ls_manager_domain),
+                'not_found'          => __('No enterprise found',$ls_manager->ls_manager_domain), 
+                'not_found_in_trash' => __('No enterprise found in trash',$ls_manager->ls_manager_domain),
+                'parent'             => __('Parent',$ls_manager->ls_manager_domain)
+            );
+            
+            register_post_type( 
+                'enterprise', 
+                array(  'labels' => $labels,
+                        'description'         => __('Enterprises listing',$ls_manager->ls_manager_domain),
+                        'public'              => true, 
+                        'publicly_queryable'  => true,
+                        'show_ui'             => true, 
+                        'exclude_from_search' => false, 
+                        'menu_position'       => 25, 
+                        'query_var'           => true,
+                        'supports'            => array( 'title', 'editor', 'thumbnail' ), 
+                        'taxonomies'          => array(''),
+                        'rewrite'             => true, 
+                        'capability_type'     => 'post', 
+                        'hierachical'         => false,
+                        'menu_icon'           => $ls_manager_globals['plugin_url'] .'/img/icon-enterprise.png',
+                )
+            );
+            
             // Post Type : Partenaire
             $labels = array(
                 'name'               => __('Partners',$ls_manager->ls_manager_domain),
