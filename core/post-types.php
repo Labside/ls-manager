3da7cdd11eed2c20f45216430e23e3fb81d5e527
Herve THOMAS <herve.thomas@labside.fr>
2014-06-05 10:05:14 +0200

    Add Quote Post Type

--- core/post-types.php
+++ core/post-types.php
@@ -212,6 +212,41 @@ if (!class_exists('LS_Manager_Post_Types')) {
                 )
             );
             
+            // Post Type : Quote
+            $labels = array(
+                'name'               => __('Quotes',$ls_manager->ls_manager_domain),
+                'singular_name'      => __('Quote',$ls_manager->ls_manager_domain), 
+                'add_new'            => __('New quote',$ls_manager->ls_manager_domain),
+                'add_new_item'       => __('Add a new quote',$ls_manager->ls_manager_domain), 
+                'edit'               => __('Edit',$ls_manager->ls_manager_domain), 
+                'edit_item'          => __('Edit quote',$ls_manager->ls_manager_domain),
+                'new_item'           => __('New quote',$ls_manager->ls_manager_domain), 
+                'view'               => __('Customers',$ls_manager->ls_manager_domain), 
+                'view_item'          => __('View quote',$ls_manager->ls_manager_domain),
+                'search_items'       => __('Search quote',$ls_manager->ls_manager_domain),
+                'not_found'          => __('No quote found',$ls_manager->ls_manager_domain), 
+                'not_found_in_trash' => __('No quote found in trash',$ls_manager->ls_manager_domain),
+                'parent'             => __('Parent',$ls_manager->ls_manager_domain)
+            );
+            
+            register_post_type( 
+                'quote', 
+                array(  'labels' => $labels,
+                        'description' => __('Quotes listing',$ls_manager->ls_manager_domain), 
+                        'public' => true, 
+                        'publicly_queryable' => true,
+                        'show_ui' => true, 
+                        'exclude_from_search' => false, 
+                        'menu_position' => 25, 
+                        'query_var' => true,
+                        'supports' => array( 'title', 'editor', 'thumbnail' ), 
+                        'taxonomies' => array(),
+                        'rewrite' => true, 
+                        'capability_type' => 'post', 
+                        'hierachical' => false,
+                        /*'menu_icon' => $ls_manager_globals['plugin_url'] .'/img/icon-customer.png',*/
+                )
+            );
         }
     
         /**
