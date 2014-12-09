63cb6745fb6980cd698a99fff171c4b04b5c332c
Herve THOMAS <herve.thomas@labside.fr>
2014-12-09 10:11:09 +0100

    Test

--- ajax/create-pdf-invoice.php
+++ ajax/create-pdf-invoice.php
@@ -8,6 +8,7 @@ if (isset($_POST['post_id'])){
     $enterprise_id = (int)$_POST['enterprise_id'];
     $dest_id       = (int)$_POST['dest_id'];
     $main_title    = (string)$_POST['main_title'];
+    $pre_paid      = (int)$_POST['pre_paid'];
     
     // Build Array Of Informations
     $data = array();
@@ -15,6 +16,7 @@ if (isset($_POST['post_id'])){
     $data['post_id']       = $post_id;
     $data['dest_id']       = $dest_id;
     $data['main_title']    = $main_title;
+    $data['pre_paid']      = $pre_paid;
 
     // Create a new PDF document.
     $pdf = new LS_Manager_PDF( $data, 'P', 'pt', 'LETTER' );
